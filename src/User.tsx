6bb41bf3894ae6f620c0d6a5ce233b406df954a8
huskyhoochu <dfg1499@gmail.com>
2021-02-03 22:07:46 +0900

    Separate profile component

--- src/User.tsx
+++ src/User.tsx
@@ -1,90 +1,30 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { createFragmentContainer, graphql } from 'react-relay';
 import { Helmet } from 'react-helmet';
 
 import { User_user } from './__generated__/User_user.graphql';
 import './User.scss';
 import ToggleDark from './ToggleDark';
+import Profile from './profile';
 
 const User: React.FC<{
   user: User_user;
   theme: 'dark' | 'light';
   setTheme: React.Dispatch<React.SetStateAction<'dark' | 'light'>>;
-}> = (props) => {
-  const { user } = props;
-
-  useEffect(() => {
-    let isMounted = true;
-
-    if (isMounted) {
-      const faviconHTML = document.getElementById('favicon');
-      faviconHTML.setAttribute('href', user.avatarUrl as string);
-    }
-
-    return () => {
-      isMounted = false;
-    };
-  }, []);
-
+}> = ({ user, theme, setTheme }) => {
   return (
     <div className="user">
       <Helmet>
+        <link
+          id="favicon"
+          rel="shortcut icon"
+          type="image/png"
+          href={user.avatarUrl as string}
+        />
         <title>{`${user.login} | Github Profile`}</title>
       </Helmet>
-      <div className="user__body">
-        <img
-          className="user__body__avatar"
-          src={user.avatarUrl as string}
-          alt={user.name}
-        />
-        <p className="user__body__name">{user.login}</p>
-        <div className="user__body__section">
-          <p className="user__body__title">Name</p>
-          <p>{user.name}</p>
-        </div>
-        <div className="user__body__section">
-          <p className="user__body__title">Bio</p>
-          <div
-            className="user__body__bio"
-            dangerouslySetInnerHTML={{ __html: user.bioHTML as string }}
-          />
-        </div>
-        <div className="user__body__section">
-          <p className="user__body__title">Email</p>
-          <a href={`mailto:${user.email}`}>{user.email}</a>
-        </div>
-        <div className="user__body__section">
-          <p className="user__body__title">Github url</p>
-          <a
-            href={user.url as string}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {user.url}
-          </a>
-        </div>
-        <div className="user__body__section">
-          <p className="user__body__title">Website</p>
-          <a
-            href={user.websiteUrl as string}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {user.websiteUrl}
-          </a>
-        </div>
-        <div className="user__body__section flex items-center justify-between">
-          <div>
-            <p className="user__body__title">팔로워</p>
-            <p>{user.followers.totalCount}명</p>
-          </div>
-          <div>
-            <p className="user__body__title">팔로잉</p>
-            <p>{user.following.totalCount}명</p>
-          </div>
-        </div>
-      </div>
-      <ToggleDark theme={props.theme} setTheme={props.setTheme} />
+      <Profile user={user} />
+      <ToggleDark theme={theme} setTheme={setTheme} />
     </div>
   );
 };
---
+++ src/profile.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { User_user } from './__generated__/User_user.graphql';
+
+import './Profile.scss';
+
+const Profile: React.FC<{ user: User_user }> = ({ user }) => {
+  return (
+    <div className="profile">
+      <img
+        className="profile__avatar"
+        src={user.avatarUrl as string}
+        alt={user.name}
+      />
+      <p className="profile__name">{user.login}</p>
+      <div className="profile__section">
+        <p className="profile__title">Name</p>
+        <p>{user.name}</p>
+      </div>
+      <div className="profile__section">
+        <p className="profile__title">Bio</p>
+        <div
+          className="profile__bio"
+          dangerouslySetInnerHTML={{ __html: user.bioHTML as string }}
+        />
+      </div>
+      <div className="profile__section">
+        <p className="profile__title">Email</p>
+        <a href={`mailto:${user.email}`}>{user.email}</a>
+      </div>
+      <div className="profile__section">
+        <p className="profile__title">Github url</p>
+        <a href={user.url as string} target="_blank" rel="noopener noreferrer">
+          {user.url}
+        </a>
+      </div>
+      <div className="profile__section">
+        <p className="profile__title">Website</p>
+        <a
+          href={user.websiteUrl as string}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {user.websiteUrl}
+        </a>
+      </div>
+      <div className="profile__section flex items-center justify-between">
+        <div>
+          <p className="profile__title">팔로워</p>
+          <p>{user.followers.totalCount}명</p>
+        </div>
+        <div>
+          <p className="profile__title">팔로잉</p>
+          <p>{user.following.totalCount}명</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
